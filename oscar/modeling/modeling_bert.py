f59cedeaa04749f8699855d3491d9923ffcf4b73
Gaurav Srivastava <gaurav.srivastava@verizonmedia.com>
2021-04-08 20:27:02 -0700

    added code to run with latest transformers

--- oscar/modeling/modeling_bert.py
+++ oscar/modeling/modeling_bert.py
@@ -187,6 +187,9 @@ class BertImgModel(BertPreTrainedModel):
         # self.apply(self.init_weights)
         self.init_weights
 
+    def get_input_embeddings(self):
+        return self.embeddings.word_embeddings
+    
     def _resize_token_embeddings(self, new_num_tokens):
         old_embeddings = self.embeddings.word_embeddings
         new_embeddings = self._get_resized_embeddings(old_embeddings, new_num_tokens)
